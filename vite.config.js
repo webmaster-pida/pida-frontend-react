0f06e0144d87edb2446d245a47e161ae6f8caa2e
PIDA <webmaster@pida-ai.com>
2026-03-05 23:53:30 +0000

    Fix vite maps

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: false, // Oculta el código original en producción
+    minify: 'esbuild', // Tritura y comprime el código al máximo
+  }
 })
